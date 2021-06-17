7441faff910150d23c981427d0d559a8ae864b88
Harsh Sharma <bcs_2019023@iiitm.ac.in>
2021-06-18 01:02:30 +0530

    add smart contract for examiners' channel

---
+++ contract/examiners/src/index.ts
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { ExamContract } from './examiners';
+
+export { ExamContract } from './examiners';
+
+export const contracts: any[] = [ExamContract];
