936bc14ef8380e1d0e4a7e83d386fcf4b1507896
Harsh Sharma <bcs_2019023@iiitm.ac.in>
2021-07-22 01:29:19 +0530

    decrypt question paper using password

--- students-app/src/routes/exams/[id]/[id].svelte
+++ students-app/src/routes/exams/[id]/[id].svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
     import { get } from "svelte/store";
+    import { navigate } from "svelte-navigator";
     import { usernameStore, identityStore } from "../../../stores/identity";
 
     export let examId: string;
     let isLoading: boolean = false;
-    let correctPassword: boolean = false;
+    let decrypted: boolean = false;
     let exam: Exam;
     let password: string;
+    let url: string;
     const username: string = get(usernameStore);
     const identityString: string = get(identityStore);
 
@@ -38,19 +40,93 @@
                 Examiner: examRes.Examiner,
                 Password: examRes.Password,
                 Address: examRes.Address,
+                ExaminerKey: examRes.ExaminerKey,
             };
-            correctPassword = true;
+            const infuraRes = await fetch(
+                "https://ipfs.infura.io:5001/api/v0/cat?" +
+                    new URLSearchParams({
+                        arg: exam.Address,
+                    }),
+                {
+                    method: "POST",
+                }
+            );
+            if (infuraRes.ok) {
+                const encryptedBlob = await infuraRes.blob();
+                const encryptedBuffer = new Uint8Array(
+                    await encryptedBlob.arrayBuffer()
+                );
+                var iterations = 10000;
+                var passwordBytes = new TextEncoder().encode(password);
+                var pbkdf2Salt = encryptedBuffer.slice(8, 16);
+
+                var passwordKey = await crypto.subtle.importKey(
+                    "raw",
+                    passwordBytes,
+                    { name: "PBKDF2" },
+                    false,
+                    ["deriveBits"]
+                );
+
+                let pbkdf2Bytes = await crypto.subtle.deriveBits(
+                    {
+                        name: "PBKDF2",
+                        salt: pbkdf2Salt,
+                        iterations,
+                        hash: "SHA-256",
+                    },
+                    passwordKey,
+                    384
+                );
+
+                pbkdf2Bytes = new Uint8Array(pbkdf2Bytes);
+                const keyBytes = pbkdf2Bytes.slice(0, 32);
+                const ivBytes = pbkdf2Bytes.slice(32);
+                const encryptedBytes = encryptedBuffer.slice(16);
+
+                let key = await crypto.subtle.importKey(
+                    "raw",
+                    keyBytes,
+                    { name: "AES-GCM", length: 256 },
+                    false,
+                    ["decrypt"]
+                );
+
+                let decryptedBytes = await crypto.subtle.decrypt(
+                    {
+                        name: "AES-GCM",
+                        iv: ivBytes,
+                    },
+                    key,
+                    encryptedBytes
+                );
+
+                decryptedBytes = new Uint8Array(decryptedBytes);
+                const blob = new Blob([decryptedBytes], {
+                    type: "application/pdf",
+                });
+                url = URL.createObjectURL(blob);
+                decrypted = true;
+            }
         } else {
-            correctPassword = false;
+            decrypted = false;
         }
         isLoading = false;
+    };
+
+    const onSubmit = () => {
+        navigate(`/exams/${examId}/submit`, {
+            state: {
+                examinerKey: exam.ExaminerKey,
+            },
+        });
     };
 </script>
 
 <main>
     {#if isLoading}
         <p>Loading...</p>
-    {:else if !correctPassword}
+    {:else if !decrypted}
         <form on:submit|preventDefault={fetchData}>
             <label for="password">Exam Password</label>
             <input
@@ -62,25 +138,13 @@
             <input type="submit" value="Open Exam" />
         </form>
     {:else}
-    <table>
-        <tr>
-            <th>ID</th>
-            <th>Date</th>
-            <th>Duration</th>
-            <th>Title</th>
-            <th>Subject</th>
-            <th>Live</th>
-            <th>Examiner</th>
-        </tr>
-            <tr>
-                <td>{exam.ID}</td>
-                <td>{new Date(exam.Date)}</td>
-                <td>{exam.Duration}</td>
-                <td>{exam.Title}</td>
-                <td>{exam.Subject}</td>
-                <td>{exam.Live ? "Yes" : "No"}</td>
-                <td>{exam.Examiner}</td>
-            </tr>
-    </table>
+        <object
+            data={url}
+            type="application/pdf"
+            title={exam.Title}
+            width="100%"
+            height="800px"
+        />
+        <button on:click={onSubmit}>Submit</button>
     {/if}
 </main>
