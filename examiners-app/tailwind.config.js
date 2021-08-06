91671a8d5e3d8d659a8adc243cf1d1dcfc1bd02e
Harsh Sharma <bcs_2019023@iiitm.ac.in>
2021-08-06 20:43:23 +0530

    add TailwindCSS configuration

---
+++ examiners-app/tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    mode: "jit",
+    purge: {
+        content: ["./src/**/*.svelte"],
+    },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                purple: {
+                    light: "#5E60CE",
+                    DEFAULT: "#6930C3",
+                    dark: "#7400B8",
+                },
+                blue: {
+                    DEFAULT: "#5390D9",
+                    light: "#4EA8DE",
+                },
+                teal: {
+                    DEFAULT: "#56CFE1",
+                },
+            },
+            boxShadow: {
+                inner: "inset 0 1px 4px 0 rgba(0,0,0,0.15)",
+            },
+        },
+    },
+    variants: {
+        extend: {
+            backgroundColor: ["checked"],
+            borderColor: ["checked"],
+        },
+    },
+    plugins: [require("@tailwindcss/forms")],
+    future: {
+        purgeLayersByDefault: true,
+        removeDeprecatedGapUtilities: true,
+    },
+};
